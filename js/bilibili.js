4838ddb2b9cb0d5ff2d043800ae0d62a854e7bee
MoukuC <2277839365@qq.com>
2020-10-07 01:47:32 +0800

    10.6

--- js/bilibili.js
+++ js/bilibili.js
@@ -1,28 +1,94 @@
-let rem =  window.innerWidth >= 1150 ? 5.2631578947368 * window.innerWidth / 100 : 60;
-const slider = document.getElementsByClassName('slider')[0];
-const body = document.getElementsByClassName('body')[0];
-const sliderMenu = document.getElementsByClassName('slider')[0].getElementsByTagName('li');
-const partitions = document.getElementsByClassName('slide');
-const menu_2 = document.getElementsByClassName('menu_2')[0];
-const optionAbove = document.getElementsByClassName('option above');
-const optionBelow = document.getElementsByClassName('option below');
-const user = document.getElementsByClassName('user')[0];
-const userData = user.getElementsByClassName('user_data')[0];
-const userImg = user.getElementsByClassName('img')[0];
 
-let sliderTop = slider.offsetTop;
 
-let timeOutEvent = null;
 
-function pxToRem(px) {
-  return px / rem;
+
+
+
+
+
+//推荐窗口的自动滚动
+setInterval(() => {
+  let recommendVedioLoop = document.getElementsByClassName('recommend')[0].getElementsByTagName('input');
+  for(let i = 0; i < recommendVedioLoop.length; i++) {
+    if(recommendVedioLoop[i].checked == true) {
+      recommendVedioLoop[i == 0 ? 3 : i - 1].checked = true;
+      break;
+    }
+  }
+}, 5000);
+
+//搜索栏基本交互
+const search = document.getElementsByClassName('search')[0];
+const searchInput = search.getElementsByTagName('input')[0];
+const searchHistory = search.getElementsByClassName('search_history')[0];
+//拷贝预设好的一个节点并删除
+let tempHistoryNode = searchHistory.getElementsByTagName('li')[0];
+const searchHistoryElem = tempHistoryNode.cloneNode(true);
+tempHistoryNode.parentNode.removeChild(tempHistoryNode);
+searchHistory.addEventListener('click', handleSearchHistoryClick);
+function handleSearchHistoryClick(event) {
+  let target = event.target;
+  let historyList = searchHistory.getElementsByTagName('li');
+  for(let i = 0; i < historyList.length; i++) {
+    if(historyList[i].contains(target)) {
+      if(historyList[i].getElementsByTagName('svg')[0].contains(target)) {
+        historyList[i].parentNode.removeChild(historyList[i]);
+      }
+    }
+  }
+  if(historyList.length == 0) {
+    searchHistory.style.display = 'none';
+  }
+}
+searchInput.addEventListener('focus', handleSearchFocus);
+function handleSearchFocus() {
+  let historyList = searchHistory.getElementsByTagName('li');
+  if(historyList.length != 0)
+    searchHistory.style.display = 'flex';
+}
+searchInput.addEventListener('keydown', handleSearchInputKeyDown);
+function handleSearchInputKeyDown(event) {
+  let keyCode = event.keyCode;
+  if(keyCode == 13) {
+    let inputStr = searchInput.value.trim();
+    searchInput.value = '';
+    if(inputStr != '') {
+      let historyList = searchHistory.getElementsByTagName('li');
+      if(historyList.length == 10) {
+        searchHistory.removeChild(historyList[historyList.length - 1])
+      }
+      if(historyList.length == 0) {
+        searchHistory.style.display = 'flex';
+      }
+      let newNode = searchHistoryElem.cloneNode(true);
+      newNode.getElementsByTagName('span')[0].innerHTML = inputStr;
+      searchHistory.insertBefore(newNode, historyList[0]);
+    }
+  }
+}
+window.addEventListener('click', handleWindowClick);
+function handleWindowClick(event) {
+  let target = event.target;
+  if(search.contains(target) == false) {
+    if(target.getAttribute('name') != 'del')
+      searchHistory.style.display = 'none';
+  }
 }
 
+
+
+//动态设置rem的值
+let rem =  window.innerWidth >= 1150 ? 5.2631578947368 * window.innerWidth / 100 : 60;
 window.onresize = handleWindowResize;
 function handleWindowResize() {
   rem =  window.innerWidth >= 1150 ? 5.2631578947368 * window.innerWidth / 100 : 60;
 }
 
+
+//用户栏移入移出动态
+const user = document.getElementsByClassName('user')[0];
+const userData = user.getElementsByClassName('user_data')[0];
+const userImg = user.getElementsByClassName('img')[0];
 user.addEventListener('mouseenter', handleUserMouseenter);
 user.addEventListener('mouseleave', handleUserMouseleave);
 function handleUserMouseleave() {
@@ -43,6 +109,12 @@ function handleUserDataMouseenter(event) {
   user.style.overflow = 'visible';
 }
 
+//中间菜单的移入移出动态
+const partitions = document.getElementsByClassName('slide');
+const menu_2 = document.getElementsByClassName('menu_2')[0];
+const optionAbove = document.getElementsByClassName('option above');
+const optionBelow = document.getElementsByClassName('option below');
+let timeOutEvent = null;
 for(let i = 0; i < optionAbove.length; i++) {
   optionAbove[i].addEventListener('mouseenter', handleOptionAboveMousenter);
   optionAbove[i].addEventListener('mouseleave', handleOptionAboveMousleave);
@@ -101,7 +173,9 @@ function handleMenu_2Mouseleave() {
 }
 
 
-
+//右侧边栏跟随滚动以及动态高亮效果
+const slider = document.getElementsByClassName('slider')[0];
+const sliderMenu = document.getElementsByClassName('slider')[0].getElementsByTagName('li');
 window.addEventListener('scroll', handleWindowScroll);
 var height = 0;
 function handleWindowScroll() {
@@ -133,4 +207,8 @@ function handleWindowScroll() {
   }
   
   height = scrollHeight;
+}
+
+function pxToRem(px) {
+  return px / rem;
 }
